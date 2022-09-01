c4268b71e580b64d701799839b43edca6f51b1b1
Sky <45232958+xkytech@users.noreply.github.com>
2022-09-01 16:54:26 +0100

    Update home.php

--- views/home.php
+++ views/home.php
@@ -11,7 +11,7 @@ require('header.php');
     <div class="card">
       <div class="card-body">
 
-        <a href="add.php" class="card-link" data-toggle="modal" data-target="#myModal">Add Todo</a>
+        <a href="" class="card-link" data-toggle="modal" data-target="#myModal">Add Todo</a>
         <a href="logout" class="card-link">Logout</a>
       </div>
     </div>
@@ -28,9 +28,9 @@ require('header.php');
               $items_todo .= '
                               <div class="card">
                               <div class="card-body">
-                                <p class="card-text">'.$item['todo'].'</p>
-                                <a href="completed?'.$item['todo_id'].'" class="card-link">completed</a>
-                                <a href="delete_this?'.$item['todo_id'].'" class="card-link">Delete</a>
+                                <p class="card-text todo_text">'.$item['todo'].'</p>
+                                 <button onclick="delete_todo(`'.$item['todo_id'].'`,`'.$item['todo'].'`)" data-toggle="modal" data-target="#delete_todo" type="button" class="delete_btn"> Delete</button>
+                                 <button onclick="done(`'.$item['todo_id'].'`,`'.$item['todo'].'`)" data-toggle="modal" data-target="#done_todo" type="button" class="done_btn"> Done</button>
                               </div>
                             </div>
                             <br>
@@ -38,7 +38,7 @@ require('header.php');
           }
       }
       else{
-          $items_todo  = "You dont have anything to do!";
+          $items_todo  = "You dont have any post yet";
       }
         
     echo   $items_todo;
@@ -73,7 +73,101 @@ require('header.php');
       </div>
     </div>
   </div>
+
+
+
+
+
+
+
   
+<div class="modal fade" id="done_todo" role="dialog">
+  <br>
+  <br>
+  <br>
+  <br>
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+        </div>
+        <div class="container">
+          <br>
+          <h3>Done?</h2>
+          <p id="done_prompt" ></p>
+          <form action="add-todo" method="post">
+            <input type="text" id="done_todo_id" value="" style="display:none">
+            <button type="submit" class="btn btn-success" style="float:right">Done</button>
+            <br>
+            <br>
+            <br>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  <br>
+<br>
+<br>
+  
+<div class="modal fade" id="delete_todo" role="dialog">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+        </div>
+        <div class="container">
+        <br>
+          <h3>Delete?</h2>
+          <p id="delete_prompt" ></p>
+          <form action="add-todo" method="post">
+            <input type="text" id="delete_todo_id" value="" style="display:none">
+            <button type="submit" class="btn btn-danger" style="float:right">Delete</button>
+            <br>
+            <br>
+            <br>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+
+
+
+
+
+
+  <script>
+    function done(id, text) {
+      document.getElementById('done_todo_id').value=id;
+      document.getElementById('done_prompt').innerHTML=text;
+    }
+
+
+    function delete_todo(id, text) {
+      console.log(id);
+      document.getElementById('delete_todo_id').value=id;
+      document.getElementById('delete_prompt').innerHTML=text;
+    }
+  </script>
 </body>
 
 </html>
